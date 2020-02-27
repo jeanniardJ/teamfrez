74079a347d481d2449cf80afd3fe9664d81f3d0a
jjeanniard <jonathanjeanniard@sfr.fr>
2020-02-27 19:07:10 +0100

    creation du systeme de login

--- src/Entity/UserComplement.php
+++ src/Entity/UserComplement.php
@@ -3,14 +3,78 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserComplementRepository")
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class UserComplement extends User
 {
+
     /**
-     * @ORM\Column(type="string", lenght=180, unique=true)
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $pseudo;
+
+    /**
+     * @ORM\Column(type="string", length=180, nullable=true)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true, nullable=true)
      */
     private $steam_id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $steamPseudo;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $battleTag;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $originPseudo;
+
+    public function getSteamPseudo(): ?string
+    {
+        return $this->steamPseudo;
+    }
+
+    public function setSteamPseudo(string $steamPseudo): self
+    {
+        $this->steamPseudo = $steamPseudo;
+
+        return $this;
+    }
+
+    public function getBattleTag(): ?string
+    {
+        return $this->battleTag;
+    }
+
+    public function setBattleTag(?string $battleTag): self
+    {
+        $this->battleTag = $battleTag;
+
+        return $this;
+    }
+
+    public function getOriginPseudo(): ?string
+    {
+        return $this->originPseudo;
+    }
+
+    public function setOriginPseudo(?string $originPseudo): self
+    {
+        $this->originPseudo = $originPseudo;
+
+        return $this;
+    }
 }
